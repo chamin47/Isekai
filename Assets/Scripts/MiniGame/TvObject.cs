7c2545fd091625dbc54f27f391bc32066b022032
chamin47 <152050575+chamin47@users.noreply.github.com>
2025-12-19 08:34:34 +0900

    .

---
+++ Assets/Scripts/MiniGame/TvObject.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+[RequireComponent(typeof(OutlineSelectSprite))]
+public class TvObject : MonoBehaviour, IPointerClickHandler
+{
+	public void OnPointerClick(PointerEventData eventData)
+	{
+		
+	}
+}
